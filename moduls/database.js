4f5bc172da358b716de90ee2ee8a2a314f70e558
Usmonjon001 <tokhirjonovusmonjon@gmail.com>
2021-08-17 20:16:56 +0500

    Exammm

---
+++ moduls/database.js
@@ -0,0 +1,93 @@
+
+const fs = require("fs/promises")
+const path = require('path')
+
+
+module.exports = class Database{
+    constructor(){
+        // kurslar uchun
+        this.kpath = path.join(__dirname, '..', '/routs', '/kurs.json')
+        this.kdata = []
+        this.readkfile()
+
+        // uquvchilar uchun
+        this.upath = path.join(__dirname, '..', '/routs', '/uquvchi.json')
+        this.udata = []
+        this.readufile()
+
+        // manbalar uchun
+        this.mpath = path.join(__dirname, '..', '/routs', '/qayer.json')
+        this.mdata = []
+        this.readmfile()
+        
+
+    }
+        // kurs class
+    async readkfile(){
+        let kdata = await fs.readFile(this.kpath, 'utf-8')
+        kdata = await JSON.parse(kdata)
+        this.kdata = kdata.kdata
+        return kdata.kdata
+        
+    }
+    async addkData(name){
+        let kdata= {
+            id: Math.random().toString(32).substring(2),
+            name
+        }
+        this.kdata.push(kdata)
+        await fs.writeFile(this.kpath, JSON.stringify({kdata:this.kdata}))
+         return kdata
+    }
+
+    // uquvchi class
+
+    async readufile(){
+        let udata = await fs.readFile(this.upath, 'utf-8')
+        udata = await JSON.parse(udata)
+         this.udata = udata.udata
+            // console.log(this.upath);
+          return udata.udata
+         
+     }
+     async adduData(name, age, kurs, manba){
+         let udata= {
+            id: this.udata.length+1,
+            name,
+            age,
+            kurs,
+            manba
+        }
+        this.udata.push(udata)
+        
+        await fs.writeFile(this.upath, JSON.stringify({udata:this.udata}))
+        
+        return udata
+    }
+
+    // manba class
+
+    async readmfile(){
+        let mdata = await fs.readFile(this.mpath, 'utf-8')
+        mdata = await JSON.parse(mdata)
+        this.mdata = mdata.mdata
+        return mdata.mdata
+        
+    }
+    async addmData(name){
+        let mdata= {
+            id: Math.random().toString(32).substring(2),
+            name,
+        }
+        this.mdata.push(mdata)
+        await fs.writeFile(this.mpath, JSON.stringify({mdata:this.mdata}))
+         return mdata
+    }
+    async delete(id){
+        let udata = await this.readufile()
+        let filtered = udata.filter(e => e.id != id)
+        await fs.writeFile( this.upath, JSON.stringify({
+            udata: filtered
+        }))
+    }
+}
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const fs = require('fs').promises
+const kursdatabase = require('./moduls/database')
+
+
+
+const db = new kursdatabase()
+// console.log(udb.addData('javohir', 'abdujalilov', 21, 'buxoro', 'web developer', 'instagram'));
+
+
+
+ const app = express();
+
+ app.listen(7000, ()=>{
+     console.log('app running port');
+ })
+ app.use(express.urlencoded({
+     extended: true,
+ }))
+app.use(express.json())
+
+ app.use(express.static(__dirname+'/public'))
+
+ app.get('/', async (req, res)=>{
+    let reed = await fs.readFile(__dirname+'/views/index.html', "utf-8")
+    res.send(reed)
+ })
+ app.get("/manager", async (req, res)=>{
+     let udata = await db.readufile();
+     res.json({
+         udata: udata
+     })
+ })
+
+ app.post("/uform", async (req, res)=>{
+   
+     let dbbody = await db.adduData(req.body.name, req.body.age, req.body.kurs, req.body.manba)
+     res.json(dbbody)
+  
+ })
+
+
+
+// kurslar bolimi
+ app.get("/kurslar", async (req, res)=>{
+    let kdata = await db.readkfile();
+    res.json({
+        kdata: kdata
+    })
+})
+app.post("/kform", async (req, res)=>{
+   
+    let dbkbody = await db.addkData(req.body.name)
+    res.json(dbkbody)
+
+})
+
+
+// manba bolimi
+app.get("/manbalar", async (req, res)=>{
+    let mdata = await db.readmfile();
+    res.json({
+        mdata: mdata
+    })
+})
+app.post("/mform", async (req, res)=>{
+   
+    let dbmbody = await db.addmData(req.body.name)
+    res.json(dbmbody)
+
+})
+ 
+
+app.delete("/delete/:id", async (req, res)=>{
+    await db.delete(req.params.id)
+    res.json({
+        ok: true,
+    })
+})
